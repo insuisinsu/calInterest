0951bc28f3eb857804857df2ce9c698a906e1db9
insuisinsu <geoinsu@gmail.com>
2023-01-13 16:45:17 +0900

    0103.2 보증금 입력 -> 전세/신용 대출금 및 이자 완성

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -1,61 +1,107 @@
 package bank;
 
 public class Bank {
-	double rate;
+	String name;
 	long limit;
-	double ratio;
 	long deposit;
-	double loan;
-	double mine;
+	double ratio;
+	double loanRate;
+	double creditRate = 8.0;
+	long loan;
+	long creditLoan;
+	long loanInterest;
+	long creditInterest;
+//	double creditLoan;
 	
-	public double getRate() {return rate;}
-	public void setRate(double rate) {this.rate = rate;}
-	public long getLimit() {return limit;}
-	public void setLimit(long limit) {this.limit = limit;}
-	public double getRatio() {return ratio;}
-	public void setRatio(double ratio) {this.ratio = ratio;}
-	public long getDeposit() {return deposit;}
-	public void setDeposit(long deposit) {this.deposit = deposit;}
-	public double getLoan() {return loan;}
-	public void setLoan(double loan) {this.loan = loan;}
-	public double getMine() {return mine;}
-	public void setMine(double mine) {this.mine = mine;}
-
 	
-	public Bank calcInterest() {
+	
+//	public double getRatio() {return ratio;}
+//	public void setRatio(double ratio) {this.ratio = ratio;}
+//	public long getLimit() {return limit;}
+//	public void setLimit(long limit) {this.limit = limit;}
+//	public double getLoanRate() {return loanRate;}
+//	public void setLoanRate(double loanRate) {this.loanRate = loanRate;}
+//	public double getCreditRate() {return creditRate;}
+//	public void setCreditRate(double creditRate) {this.creditRate = creditRate;}
+//	public long getDeposit() {return deposit;}
+//	public void setDeposit(long deposit) {this.deposit = deposit;}
+//	public double getLoan() {return loan;}
+//	public void setLoan(double loan) {this.loan = loan;}
+//	public double getMine() {return mine;}
+//	public void setMine(double mine) {this.mine = mine;}
+//	public double getCreditLoan() {return creditLoan;}
+//	public void setCreditLoan(double creditLoan) {this.creditLoan = creditLoan;}
+	
+	@Override
+	public String toString() {
+		return "은행:" + this.name + "  deposit:" + this.deposit + "  loan:" + this.loan + "  mine:" + this.creditLoan;
+	}
+	
+	/**
+	 * deposit 을 가지고 은행 조건에 맞추어 이자를 계산하여 Bank 에 담는다.
+	 * @param deposit
+	 * @return 
+	 */
+	public Bank calcInterest(long deposit) {
+		this.deposit = deposit;
+		this.splitDeposit(deposit);
+		this.calcInterest();
 		return this;
 	}
-	
-	public boolean chkDeposit() {
-		
-		return true;
+	/**
+	 * deposit 을 loan 과 mine 에 나누어 담기
+	 * 단. 은행의 보증금 비율(ratio) 와 한도(limit) 를 고려함
+	 * @param deposit
+	 */
+	private void splitDeposit(long deposit) {
+		if((deposit * this.ratio) > this.limit) {
+			this.loan = this.limit;
+			this.creditLoan = deposit - this.limit;
+		}else {
+			this.loan = (long) (deposit * this.ratio);
+			this.creditLoan = this.deposit - this.loan;
+		}
 	}
-	
-	public double getTotalInterest(long loan) {
-		double interest = 12.34;
-		
-		System.out.println("123123   "+this.limit);
-		
-		return interest;
+	/**
+	 * 연이자 계산
+	 */
+	private void calcInterest() {
+		this.setLoanInterest();
+		this.setcMineInterest();
+	}
+	/**
+	 * 전세대출(loan)의 연이자 계산
+	 */
+	private void setLoanInterest() {
+		this.loanInterest = (long) (this.loan * this.loanRate);
+	}
+	/**
+	 * 신용대출(mine)의 연이자 계산
+	 */
+	private void setcMineInterest() {
+		this.creditInterest = (long) (this.creditLoan * this.creditRate);
 	}
 }
 class LH extends Bank {
 	public LH() {
-		this.rate = 0.012;
+		this.name = "LH";
+		this.loanRate = 0.012;
 		this.limit = 100000000;
 		this.ratio = 0.8;
 	}
 }
 class KakaoSalary extends Bank {
 	public KakaoSalary() {
-		this.rate = 0.028;
+		this.name = "KakaoSalary";
+		this.loanRate = 0.028;
 		this.limit = 222000000;
 		this.ratio = 0.8;
 	}
 }
 class KakaoYouth extends Bank {
 	public KakaoYouth() {
-		this.rate = 0.028;
+		this.name = "KakaoYouth";
+		this.loanRate = 0.028;
 		this.limit = 70000000;
 		this.ratio = 0.9;
 	}
--- src/bank/calcInterest.java
+++ src/bank/calcInterest.java
@@ -1,17 +1,39 @@
 package bank;
 
+import java.util.Scanner;
+
 public class calcInterest {
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		long deposit = sc.nextLong();
+		long mine = 0;
+		sc.close();
+		
 		Bank lh = new LH();
 		Bank kakaoSalary = new KakaoSalary();
+		Bank kakaoYouth = new KakaoYouth();
 		
-		Bank[] bankList = new Bank[2];
+		Bank[] bankList = new Bank[3];
 		bankList[0] = lh;
 		bankList[1] = kakaoSalary;
-
+		bankList[2] = kakaoYouth;
+		
+		// 실행
 		for(int i = 0; i < bankList.length; i++) {
-			System.out.println(bankList[i].rate);
+			bankList[i].calcInterest(deposit);
+		}
+		// 출력
+		System.out.printf("전세금 : "+deposit +"   보유금 : "+mine);
+		System.out.println();
+		System.out.println("-------------------------------------------------------------------------------------------------------");
+		System.out.printf("%16s %15s %15s %15s %15s %15s", "bank", "loan", "loanInterest", "credit", "creditInterest", "TotalInterest");
+		System.out.println();
+		System.out.println("-------------------------------------------------------------------------------------------------------");
+		for(int i = 0; i < bankList.length; i++) {
+			System.out.printf("%-15s %1s %15d %15d %15d %15d %15d",bankList[i].name, ":",bankList[i].loan, bankList[i].loanInterest/12
+					, bankList[i].creditLoan, bankList[i].creditInterest/12, (bankList[i].loanInterest + bankList[i].creditInterest)/12);
+			System.out.println();
 		}
 		
 	}
